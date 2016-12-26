3cdca6c31e8de76a3ead8876b4c2f2834e7be108
Sebastian Suttner <Sebastian.Suttner@careerbuilder.com>
2016-12-26 16:59:24 -0300

    AT-20113 Don't notify ActionController::ParameterMissing exception

--- templates/config/initializers/exception_notification.rb
+++ templates/config/initializers/exception_notification.rb
@@ -1,6 +1,7 @@
 unless Rails.env.test? || Rails.env.development?
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
+    ignore_exceptions: ['ActionController::ParameterMissing'] + ExceptionNotifier.ignored_exceptions,
     email: {
       email_prefix: "[my_app_name #{Rails.env.upcase}] ",
       sender_address: %{"CB Notifier" <notifier.cb@gmail.com>},
