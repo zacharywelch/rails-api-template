c246398f0fb77550428634309b7ebd3e3801bd20
aouzounov <Anton.Ouzounov@careerbuilder.com>
2017-01-20 11:13:49 -0500

    AT-20357 Adding application revision to health

--- templates/config/initializers/okcomputer.rb
+++ templates/config/initializers/okcomputer.rb
@@ -1,2 +1,5 @@
 # Mount healcheck url at '/health'
 OkComputer.mount_at = 'health'
+
+# Mount the application revision check
+OkComputer::Registry.register 'app_version', OkComputer::AppVersionCheck.new
